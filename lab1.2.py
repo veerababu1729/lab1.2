997c566473d3a4a3b899e1de73ee8683e87450b4
veerababu1729 <84015665+veerababu1729@users.noreply.github.com>
2021-05-28 19:50:37 +0530

    Add files via upload

---
+++ lab1.2.py
@@ -0,0 +1,7 @@
+a=float(input('enter first value:'))
+b=float(input('enter second value:'))
+c=float(input('enter third value:'))
+d=a+b+c
+e=(a+b+c)/3
+print('sum of the three numbers is:{}'.format(d))
+print('average of three numbers is:{}'.format(e))
